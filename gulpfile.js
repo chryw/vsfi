1d9635c0c9c689dac2e6372318ce8d14c4cb479e
Cherry Wang <cherryregister@gmail.com>
2015-09-30 20:00:48 +0000

    revised gulpfile to generate icon font and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,34 @@
 var gulp = require("gulp");
-var rename = require("gulp-rename");
 var iconfont = require('gulp-iconfont');
-var consolidate = require('gulp-consolidate');
+var iconfontCss = require('gulp-iconfont-css');
 var runTimestamp = Math.round(Date.now() / 1000);
 
 var fontName = 'vsfi'; // set name of your symbol font
-var template = 'fontawesome-style'; // you can also choose 'foundation-style'
 gulp.task('iconfont', function() {
   gulp.src(['assets/icons/*.svg'])
-    .pipe(iconfont({
+    .pipe(iconfontCss({
       fontName: fontName,
-      appendUnicode: true, // recommended option
-      formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'], // default, 'woff2' and 'svg' are available
-      timestamp: runTimestamp, // recommended to get consistent builds when watching files
+      targetPath: '../../assets/css/vsfi.css',
+      fontPath: '../fonts/'
     }))
+    .pipe(iconfont({fontName: fontName}))
     .on('glyphs', function(glyphs) {
       var options = {
         glyphs: glyphs.map(function(glyph) {
           // this line is needed because gulp-iconfont has changed the api from 2.0
           return {
+            appendUnicode: true, // recommended option
+            timestamp: runTimestamp, // recommended to get consistent builds when watching files
             name: glyph.name,
-            codepoint: glyph.unicode[0].charCodeAt(0)
+            codepoint: glyph.unicode[0].charCodeAt(0).toString(16).toUpperCase(),
+            normalize: true,
+            fontHeight: 5000,
           }
         }),
         fontName: fontName,
         fontPath: 'assets/fonts/', // set path to font (from your CSS file if relative)
-        className: 'vsfi' // set class name in your CSS
+        className: fontName // set class name in your CSS
       };
-      // CSS templating
-      gulp.src('templates/' + template + '.css')
-        .pipe(consolidate('lodash', options))
-        .pipe(rename({
-          basename: fontName
-        }))
-        .pipe(gulp.dest('.')); // set path to export your CSS
-        // HTML templating
-      gulp.src('templates/' + template + '.html')
-        .pipe(consolidate('lodash', options))
-        .pipe(rename({ basename:fontName}))
-        .pipe(gulp.dest('/')); // set path to export your sample HTML
       console.log(glyphs);
     })
     .pipe(gulp.dest('assets/fonts/')); // set path to export your fonts
