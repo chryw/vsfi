2c79e0c4f0b9b8d7b90d33b98d5f61f3f152e956
Cherry Wang (Aquent LLC) <v-cherrw@microsoft.com>
2017-08-03 18:05:01 -0700

    Changed copy task; added link to mapping page

--- gulpfile.js/copy.js
+++ gulpfile.js/copy.js
@@ -4,4 +4,6 @@ const gulp = require('gulp');
 gulp.task('copy', ['cachebust'], () => {
   gulp.src(['dist/_IconsCommon.scss', 'dist/Bowtie.eot', 'dist/Bowtie.svg', 'dist/Bowtie.woff'])
     .pipe(gulp.dest('dist/VSO'));
+  gulp.src(['dist/Bowtie.eot', 'dist/Bowtie.svg', 'dist/Bowtie.ttf', 'dist/Bowtie.woff', 'dist/FabMDL2.eot', 'dist/FabMDL2.svg', 'dist/FabMDL2.ttf', 'dist/FabMDL2.woff', 'dist/FabMDL2.woff2', 'dist/index.html', 'dist/mapping.html', 'dist/style.css'])
+    .pipe(gulp.dest('dist/catalog'));
 });
